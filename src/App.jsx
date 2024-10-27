e2824ccbb2bb5434cb2ea37aae7e47a2c863f0d8
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2024-10-27 23:44:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NavBer from './components/Navber'
+
+function App() {
+
+
+  return (
+    <>
+      <NavBer></NavBer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBer.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { MdClose } from "react-icons/md";
+const NavBer = () => {
+    const [open, setOpen] = useState(true)
+    const route = [
+        { id: 1, path: '/', name: "Home" },
+        { id: 2, path: '/about', name: "About" },
+        { id: 3, path: '/services', name: "Services" },
+        { id: 4, path: '/Contact', name: "Contact" },
+        { id: 5, path: '*', name: "NotFount" },
+    ]
+
+
+    return (
+
+        <nav className="bg-white shadow-md py-10">
+            <div
+                className="text-3xl ml-10 lg:hidden"
+                onClick={() => setOpen(!open)}>
+                {
+                    open === true ?
+                        <CiMenuBurger />
+                        :
+                        <MdClose />
+                }
+            </div>
+
+
+            <ul className={`md:hidden  absolute mt-3 lg:hidden
+        ${open ? '-left-32' : "  "}
+             bg-slate-700 rounded-md duration-1000 left-4 shadow-2xl text-white p-5`} >
+                {
+                    route.map(nav =>
+                        <li
+                            className=""
+                            key={nav.id}
+                        >
+                            <a
+                                href={nav.path}
+                            >
+                                {nav.name}
+                            </a>
+                        </li>)
+                }
+            </ul>
+
+
+
+
+        </nav>
+
+    );
+};
+
+export default NavBer;
